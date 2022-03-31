d3d903fdedfbbaaa20943d2622190ecf75ccb10f
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-31 14:59:49 +0200

    added controller for Lists, created methods for lists controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,36 @@
 class ListsController < ApplicationController
-	
+	def index
+		@lists = List.all
+	end
+
+	def show
+		@list = List.find(params[:id])
+	end
+
+# instantiate the form
+	def new
+		@list = List.new
+	end
+
+	def create
+		if
+			@list = List.create(list_params)
+			redirect_to list_path(@list)
+		else
+			render :new
+		end
+
+	end
+
+	def destroy
+		@list = List.find(params[:id])
+		@list.destroy
+		redirect_to lists_path
+	end
+
+	private
+#whitelisting for security
+	def list_params
+	params.require(:list).permit(:name)
+	end
 end
